2aa1d8bdf8e3a2173286d86872b476275d2e1469
Elias Axonov <42652031+ellegamee@users.noreply.github.com>
2020-10-10 18:07:51 +0200

    Create ChooseRandom.py

---
+++ Experiment/ChooseRandom.py
@@ -0,0 +1,27 @@
+import random
+
+words = []
+
+# Open file where all the words are
+# ! THIS JUST WORKS ON LAPTOP
+# ! DON'T TRY ON STATIONARY
+
+myfile = open(
+    r"C:\Users\User\Documents\Coding\Python\School\Kapitel6\Hangman\Wordsrandom.txt",
+    "rt",
+    encoding="utf8",
+)
+
+# Reads idividually every line and puts in LST "words"
+for i in range(221599):
+    content = myfile.readline()
+    words.append(content)
+myfile.close
+
+# Chose a random word and remove "/n" in the end
+whole_word = random.choice(words)
+master_word = whole_word[:-1]
+
+# TODO: Make the master_word split up into each character
+# ! Not done at the end, maybe works
+print(master_word.split())
