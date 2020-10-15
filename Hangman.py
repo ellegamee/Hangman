a8cb2e0ab1390e9126eb9d7dc48296cef0c9bee2
Elias Axonov <ed.axonov@gmail.com>
2020-10-15 14:01:30 +0200

    Update Hangman.py
    Small improvements and develops everywhere. All menus got their own text docuements to make it eisier to change the look.

--- Hangman.py
+++ Hangman.py
@@ -1,26 +1,27 @@
 import random, time, os, sys
 
-loopbool = True
-while loopbool:
+menu_loop = True
+while menu_loop:
 
-    # TODO Print out the main menu
-    # TODO Make the output print (CODE) prettier
-    print(
-        "This is the main menu, select by typing wich option you want.\n\n"
-        "===============================\n"
-        "|                             |\n"
-        "|   Play    Rules    Exit     |\n"
-        "|                             |\n"
-        "===============================\n",
+    # Reads the main menu graphics
+    main_menu = open(
+        r"Graphics\Main_menu.txt",
+        "rt",
+        encoding="utf8",
     )
+
+    # Prints and closes main menu txt document
+    content = main_menu.read()
+    main_menu.close()
+    print(content)
 
     # TODO: Make the menu input all lower case: play = Play
     # TODO: Overall improve the menu structure, loop maybe?
-    menu_choise = input()
+    menu_choise = input("Choose: ").lower()
     if menu_choise == "play":
 
         # Stop loop and clear terminal
-        loopbool = False
+        menu_loop = False
         os.system("cls" if os.name == "nt" else "clear")
 
         # TODO Continue to choose difficultie
@@ -28,17 +29,17 @@ while loopbool:
 
     elif menu_choise == "rules":
 
-        # Stop loop and clear terminal
-        loopbool = False
+        # clear terminal
         os.system("cls" if os.name == "nt" else "clear")
 
-        # TODO: Print out the rules
+        # TODO: Write the rules in the txt documnt
         print("This will print the rules.")
+        input("Press Enter to continue...")
+        os.system("cls" if os.name == "nt" else "clear")
 
     elif menu_choise == "exit":
 
-        # Stop loop and clear terminal
-        loopbool = False
+        # clear terminal
         os.system("cls" if os.name == "nt" else "clear")
 
         # Goodbye greeting to coustomer
@@ -49,17 +50,37 @@ while loopbool:
         # Stop loop and clear terminal
         os.system("cls" if os.name == "nt" else "clear")
 
-        print("Welcome to the dev menu.")
-        print("Type: back to come to the main menu again")
+        # Reads the main menu graphics
+        dev_menu = open(
+            r"Graphics\Dev_menu.txt",
+            "rt",
+            encoding="utf8",
+        )
+
+        # Prints and closes dev txt document
+        content = dev_menu.read()
+        dev_menu.close()
+        print(content)
 
         while True:
-            devmenu_choise = input("\nCommand: ")
+
+            # Decide option
+            devmenu_choise = input("\nCommand: ").lower()
+
+            # Check what to do
             if devmenu_choise == "back":
                 os.system("cls" if os.name == "nt" else "clear")
                 break
+
+            # TODO When at the right moment fix win and so on
+            elif devmenu_choise == "win":
+                print("This does not work at the moment right now.")
+            elif devmenu_choise == "lose":
+                print("This does not work at the moment right now.")
             else:
-                print("There are no more options at the moment")
+                print("Wrong option, try again.")
 
     else:
+        print("Error, choose an real option!")
+        time.sleep(1.5)
         os.system("cls" if os.name == "nt" else "clear")
-        print("error, choose an real option")
