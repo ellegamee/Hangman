073a1f046c6dd96c6ec53fd4aa856a9696f7af79
Elias Axonov <ed.axonov@gmail.com>
2020-10-19 15:46:19 +0200

    Update Hangman.py
    Spelling and work to start the main game so I can continue the work and be finished.

--- Hangman.py
+++ Hangman.py
@@ -9,22 +9,68 @@ while menu_loop:
         "rt",
         encoding="utf8",
     )
-
-    # Prints and closes main menu txt document
     main_menu = main_doc.read()
     main_doc.close()
     print(main_menu)
 
-    menu_choise = input("Choose: ").lower()
+    # Reads and closes the difficulties rules
+    difficultiesrule_doc = open(
+        r"Graphics\Difficultiesrule.txt",
+        "rt",
+        encoding="utf8",
+    )
+    difficultiesrule = difficultiesrule_doc.read()
+    difficultiesrule_doc.close()
+
+    menu_choice = input("Choose: ").lower()
     os.system("cls" if os.name == "nt" else "clear")
 
-    if menu_choise == "play":
+    if menu_choice == "play":
         menu_loop = False
 
-        # TODO Continue to choose difficultie
-        print("Continue with choosing difficultie.")
+        difficulties_doc = open(
+            r"Graphics\Difficulties.txt",
+            "rt",
+            encoding="utf8",
+        )
 
-    elif menu_choise == "rules":
+        # Prints and closes difficulties menu and txt document
+        difficulties_menu = difficulties_doc.read()
+        difficulties_doc.close()
+        os.system("cls" if os.name == "nt" else "clear")
+
+        while True:
+            # Input and help
+            print(difficulties_menu)
+            print("For info type help")
+            difficulties_choice = input("Choose: ").lower()
+            os.system("cls" if os.name == "nt" else "clear")
+
+            if difficulties_choice == "help":
+                print(difficultiesrule)
+                input("Press Enter to go back... ")
+                os.system("cls" if os.name == "nt" else "clear")
+
+            elif difficulties_choice == "easy" or "medium" or "hard" or "extreme":
+
+                difficulties = {
+                    "easy": {"life's": 10, "animation": "first"},
+                    "medium": {"life's": 8, "animation": "second"},
+                    "hard": {"life's": 6, "animation": "third"},
+                    "extreme": {"life's": 4, "animation": "fourth"},
+                }
+
+                # * This is a test of how many lives
+                # difficulties[difficulties_choice]["life's"]
+
+                # gets the master word from the computer
+
+            else:
+                print("\n   Error, choose a real option!")
+                time.sleep(2)
+                os.system("cls" if os.name == "nt" else "clear")
+
+    elif menu_choice == "rules":
 
         # ! Major text wrong needs to get a look at
         gameplay_doc = open(
@@ -43,31 +89,23 @@ while menu_loop:
         rulemenu = rulemenu_doc.read()
         rulemenu_doc.close()
 
-        difficultiesrule_doc = open(
-            r"Graphics\Difficultiesrule.txt",
-            "rt",
-            encoding="utf8",
-        )
-        difficultiesrule = difficultiesrule_doc.read()
-        difficultiesrule_doc.close()
-
         while True:
             # prints rule menu, choose menu, clear menu
             print(rulemenu)
-            rulemenu_choise = input("Choose: ").lower()
+            rulemenu_choice = input("Choose: ").lower()
             os.system("cls" if os.name == "nt" else "clear")
 
-            if rulemenu_choise == "gameplay":
+            if rulemenu_choice == "gameplay":
                 print(gameplay)
                 input("Press Enter to go back... ")
                 os.system("cls" if os.name == "nt" else "clear")
 
-            elif rulemenu_choise == "difficulties":
+            elif rulemenu_choice == "difficulties":
                 print(difficultiesrule)
                 input("Press Enter to go back... ")
                 os.system("cls" if os.name == "nt" else "clear")
 
-            elif rulemenu_choise == "back":
+            elif rulemenu_choice == "back":
                 os.system("cls" if os.name == "nt" else "clear")
                 break
 
@@ -76,15 +114,15 @@ while menu_loop:
                 time.sleep(2)
                 os.system("cls" if os.name == "nt" else "clear")
 
-    elif menu_choise == "exit":
+    elif menu_choice == "exit":
 
-        # Goodbye greeting to coustomer
+        # Goodbye greeting to customer
         print("Good bye, have a great day.")
         sys.exit()
 
-    elif menu_choise == "dev":
+    elif menu_choice == "dev":
 
-        # Reads the main menu graphics
+        # Reads the dev menu graphics
         devmenu_doc = open(
             r"Graphics\Dev_menu.txt",
             "rt",
@@ -98,19 +136,19 @@ while menu_loop:
         while True:
 
             # Decide option
-            devmenu_choise = input("\nCommand: ").lower()
+            devmenu_choice = input("\nCommand: ").lower()
             print(devmenu)
 
             # Check what to do
-            if devmenu_choise == "back":
+            if devmenu_choice == "back":
                 os.system("cls" if os.name == "nt" else "clear")
                 break
 
             # TODO When at the right moment fix win and so on
-            elif devmenu_choise == "win":
+            elif devmenu_choice == "win":
                 print("This does not work at the moment right now.")
 
-            elif devmenu_choise == "lose":
+            elif devmenu_choice == "lose":
                 print("This does not work at the moment right now.")
 
             else:
