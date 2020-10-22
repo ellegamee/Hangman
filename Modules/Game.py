d059cbd895894e3944d4d0ab5af0b24c4b40050e
Elias Axonov <ed.axonov@gmail.com>
2020-10-22 12:55:45 +0200

    Bug fixing

--- Modules/Game.py
+++ Modules/Game.py
@@ -43,34 +43,38 @@ def gameplay():
             hidden_characters = list("_" * len(characters))
             lifes = difficulties[difficulties_choice]["life's"]
 
+            # Character preperation
+            wrong_characters = []
+
             # TODO: For the future make the hanging man print
             # * Main game loop
             game = True
             while game:
 
                 # Cheat to develop the program
-                print(whole_word)
-                print("".join(hidden_characters))
+                # print(whole_word)
+
                 # Life and characters left with input
                 print(" " + " ".join(hidden_characters) + "\n")
-                print("You have {} life's left".format(lifes))
+                print(" " + ", ".join(wrong_characters))
+                print("\nYou have {} life's left".format(lifes))
                 character_guess = input("Guess on character: ")
 
                 # TODO: Make This better
                 # * First test of character guess
                 correct_guesses = 0
                 for i in characters:
-                    if character_guess == i:
+                    if character_guess.lower() == i:
                         correct_guesses += 1
 
                 # If you get it right
                 if correct_guesses > 0:
                     os.system("cls" if os.name == "nt" else "clear")
-                    print("You found {} characters!".format(correct_guesses))
+                    print("\n You found {} characters!\n".format(correct_guesses))
 
                     for y in range(correct_guesses):
-                        position = characters.index(character_guess)
-                        hidden_characters[position] = character_guess
+                        position = characters.index(character_guess.lower())
+                        hidden_characters[position] = character_guess.lower()
                         characters[position] = " "
 
                     # See if there are any "_" left
@@ -83,21 +87,27 @@ def gameplay():
                     if dash_count == 0:
                         os.system("cls" if os.name == "nt" else "clear")
 
+                        game = False
                         Endscreen.endscreen("\n     Congrats you won!!!")
 
                 # When you found no character
                 else:
                     os.system("cls" if os.name == "nt" else "clear")
-                    print("\n You found no characters, you loose one life.")
+                    print("\n You found no characters, you loose one life.\n")
                     lifes -= 1
+
+                    # Saves wrong character
+                    wrong_characters.append(character_guess.lower())
 
                     # Check if you have lost
                     if lifes <= 0:
                         os.system("cls" if os.name == "nt" else "clear")
                         print(" You have lost!")
                         print(" The correct word was: {}".format(whole_word))
+                        time.sleep(2)
                         os.system("cls" if os.name == "nt" else "clear")
 
+                        game = False
                         Endscreen.endscreen("\n     You lost, nice try!")
 
         else:
